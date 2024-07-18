c231ca58acd1726870717c322bc8440914f3c2df
Ibrar Ansari <93324779+meibraransari@users.noreply.github.com>
2024-07-18 14:22:34 +0530

    Create Download.sh

---
+++ Download.sh
@@ -0,0 +1,31 @@
+cat urls.txt | while read -e l
+do
+	wget -c "$l"
+done
+
+
+cat urls.txt 
+http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso
+http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-i386.iso
+http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-dvd/debian-8.6.0-amd64-DVD-1.iso
+http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-dvd/debian-8.6.0-amd64-DVD-2.iso
+http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-dvd/debian-8.6.0-amd64-DVD-3.iso
+http://ftp.jaist.ac.jp/pub/Linux/Fedora/releases/24/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-24-1.2.iso
+http://mirrors.kernel.org/linuxmint//stable/18/linuxmint-18-mate-64bit.iso
+http://mirrors.kernel.org/linuxmint//stable/18/linuxmint-18-cinnamon-64bit.iso
+https://download.fedoraproject.org/pub/fedora/linux/releases/24/Spins/x86_64/iso/Fedora-KDE-Live-x86_64-24-1.2.iso
+http://cdimage.kali.org/kali-2016.2/kali-linux-2016.2-amd64.iso
+https://download.fedoraproject.org/pub/alt/releases/24/Labs/x86_64/iso/Fedora-Scientific_KDE-Live-x86_64-24-1.2.iso
+http://isoredirect.centos.org/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1511.iso
+https://download.fedoraproject.org/pub/alt/releases/24/Labs/x86_64/iso/Fedora-Security-Live-x86_64-24-1.2.iso
+https://download.fedoraproject.org/pub/alt/releases/24/Labs/x86_64/iso/Fedora-Robotics-Live-x86_64-24-1.2.iso
+http://download.opensuse.org/distribution/leap/42.2/iso/openSUSE-Leap-42.2-DVD-x86_64-Build0215-Media.iso
+http://mirrors.slackware.com/slackware/slackware-iso/slackware64-14.2-iso/slackware64-14.2-install-dvd.iso
+http://cdimage.ubuntu.com/kubuntu/releases/16.10/release/kubuntu-16.10-desktop-amd64.iso.torrent
+http://mirror.fibergrid.in/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1511.iso
+ftp://mirror.bosslinux.in/ISO%20Images/EduBOSS/Eduboss-4.0/EduBOSS-4.0-i386-19Nov2015.iso
+http://cdimage.ubuntu.com/ubuntu-gnome/releases/16.10/release/ubuntu-gnome-16.10-desktop-amd64.iso
+https://peppermintos.com/iso/Peppermint-7-20160616-amd64.iso
+http://spout.ussg.indiana.edu/linux/pclinuxos/pclinuxos/live-cd/64bit/pclinuxos64-kde-2016.03.iso
+https://sourceforge.net/projects/reactos/files/ReactOS/0.4.2/ReactOS-0.4.2-iso.zip/download
+https://sourceforge.net/projects/archbang/files/ArchBang-Curve/archbang-curve-rc-191016-x86_64.iso/download
