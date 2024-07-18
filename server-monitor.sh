a894090693ccdc1aca82448e309cd603004270fb
Ibrar Ansari <93324779+meibraransari@users.noreply.github.com>
2024-07-18 14:19:56 +0530

    Create server-monitor.sh

---
+++ server-monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SERVERIP=192.168.2.3
+NOTIFYEMAIL=test@example.com
+
+ping -c 3 $SERVERIP > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+   # Use your favorite mailer here:
+   mailx -s "Server $SERVERIP is down" -t "$NOTIFYEMAIL" < /dev/null 
+fi
